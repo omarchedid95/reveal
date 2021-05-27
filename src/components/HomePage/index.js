cd4006f428934c423521aa8af875d037d6bcfda3
Omar Chedid <ochedid@twitter.com>
2021-05-26 22:17:46 -0700

    Implemented a basic home page. Embedded the nav inside this page.:

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,35 @@
+import { Button, Divider, Hidden } from '@material-ui/core'
 import React, { Component } from 'react'
+import BottomMenu from '../BottomMenu';
+import { withAuth } from '../HOC'
+import NavBar from '../NavBar';
 
-export default class HomePage extends Component {
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.logout = props.auth.logout;
+    }
+    handleLogout = () => {
+        this.logout().then(res => console.log(res)).catch(err => console.log(err))
+    }
     render() {
+        const user = this.props.auth.currentUser;
         return (
             <div>
-                Home Page
+                <Hidden xsDown>
+                    <NavBar />
+                </Hidden>
+                {
+                    JSON.stringify(user)
+                }
+                <Divider />
+                <Button onClick={this.handleLogout}>Logout</Button>
+
+                <Hidden smUp>
+                    <BottomMenu />
+                </Hidden>
             </div>
         )
     }
 }
+export default withAuth(HomePage);
