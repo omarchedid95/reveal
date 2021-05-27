66a21ea8c0c553176c19a47074bd731d509d4725
Omar Chedid <ochedid@twitter.com>
2021-05-26 22:18:05 -0700

    Implemented a basic login page

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,10 +1,19 @@
 import { Button } from '@material-ui/core'
 import React, { Component } from 'react'
-
-export default class LoginPage extends Component {
+import { withRouter } from 'react-router';
+import { withAuth } from '../HOC';
+class LoginPage extends Component {
+    handleLogin = () => {
+        this.props.auth.signInWithGoogle().then((res) => console.log(res)).catch((err) => console.log(err))
+    }
     render() {
         return (
-            <Button>LOGIN</Button>
+            <div>
+                <Button onClick={() => this.props.history.push('/')}>Go to landing</Button>
+                <Button onClick={() => this.props.history.push('/signup')}>Go to signup</Button>
+                <Button onClick={this.handleLogin}>Login with Google</Button>
+            </div>
         )
     }
 }
+export default withRouter(withAuth(LoginPage));
