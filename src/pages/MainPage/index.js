ea8f8bbc7169954cca1fef7b13fa2e18bd2b5fe4
Omar Chedid <ochedid@twitter.com>
2021-05-28 19:43:55 -0700

    Finalized top level app styling

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,23 +1,19 @@
-import { Button, Divider, Hidden, Paper } from '@material-ui/core'
 import React, { Component } from 'react'
 import SideMenu from '../../components/SideMenu';
-import BottomMenu from '../../components/BottomMenu';
 import { withAuth } from '../../components/HOC'
-import MatchesList from '../../components/MatchesList';
-import NavBar from '../../components/NavBar';
-import './index.css';
-import { Route, Switch } from 'react-router';
 import Chat from '../../components/Chat';
 import ProfilePage from '../ProfilePage';
-class HomePage extends Component {
-    
+import { Route, Switch } from 'react-router';
+import './index.css';
+
+class MainPage extends Component { 
     render() {
         return (
-            <div className='home-page-component-wrapper'>
-                <aside className='home-page-aside'>
+            <div className='main-page-component-wrapper'>
+                <aside>
                     <SideMenu />
                 </aside>
-                <main className='home-page-main-section'>
+                <main>
                     <Switch>
                         <Route exact path='/app' component={() => <Chat />} />
                         <Route exact path='/app/profile' component={() => <ProfilePage />} />
@@ -27,4 +23,4 @@ class HomePage extends Component {
         )
     }
 }
-export default withAuth(HomePage);
+export default withAuth(MainPage);
